728f8363ccd4edf8ef36edbe530fc588b11b0ec7
sanny-io <sannysherief@gmail.com>
2020-03-02 14:49:42 -0800

    Progress.

---
+++ api.py
@@ -0,0 +1,138 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+base_url = "https://lambda-treasure-hunt.herokuapp.com/api/adv/"
+
+headers = {
+    "Authorization": f"Token {os.getenv('API_KEY')}"
+}
+
+
+def init():
+    response = requests.get(f"{base_url}init/", headers=headers)
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def move(direction, next_room_id=None):
+    dictionary = {"direction": direction}
+
+    if next_room_id:
+        dictionary["next_room_id"] = next_room_id
+
+    response = requests.post(
+        f"{base_url}move/", headers=headers, json=dictionary)
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def take_item(item_name):
+    response = requests.post(
+        f"{base_url}take/", headers=headers, json={"name": item_name})
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def drop_item(item_name):
+    response = requests.post(
+        f"{base_url}drop/", headers=headers, json={"name": item_name})
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def status():
+    response = requests.post(f"{base_url}status/", headers=headers)
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def wear_item(item_name):
+    response = requests.post(
+        f"{base_url}wear/", headers=headers, json={"name": item_name})
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def undress_item(item_name):
+    response = requests.post(f"{base_url}undress/",
+                             headers=headers, json={"name": item_name})
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def examine(name):
+    response = requests.post(f"{base_url}examine/",
+                             headers=headers, json={"name": name})
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def pray():
+    response = request.post(f"{base_url}pray/", headers=headers)
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def change_name(name):
+    response = requests.post(
+        f"{base_url}change_name/", headers=headers, json={"name": name})
+    json = response, json()
+    print(response.text)
+    return json
+
+
+def fly(direction):
+    response = request.post(
+        f"{base_url}fly/", headers=headers, json={"direction": direction})
+    json = response.json()
+    print(response.text)
+    return json
+
+# def dash(direction, num_rooms=None, next_room_ids=None):
+    # dictionary = {"direction": direction}
+
+    # if next_room_ids:
+    #     dictionary["next_room_ids"] = next_room_ids
+
+    # response = requests.post(
+    #     f"{base_url}dash/", headers=headers, json=dictionary)
+    # json = response.json()
+    # print(response.text)
+    # return json
+
+# def carry(name):
+#     response = request.post(f"{base_url}carry/", headers=headers, json= {"name": name})
+# json = response.json()
+# print(response.text)
+# return json
+
+# def receive():
+#   response = request.post(f"{base_url}receive/, headers=headers)
+# json = response.json()
+# print(response.text)
+# return json
+
+
+def warp():
+    response = request.post(f"{base_url}warp/", headers=headers)
+    json = response.json()
+    print(response.text)
+    return json
+
+
+def recall():
+    response = request.post(f"{base_url}recall/", headers=headers)
+    json = response.json()
+    print(response.text)
+    return json
--- explorer.py
+++ explorer.py
@@ -1,5 +1,54 @@
-import os
-from dotenv import load_dotenv
+import random
+import api
+import time
+import json
+from util import Stack
 
-load_dotenv()
-api_key = os.getenv("API_KEY")
+opposite_direction_mapping = {
+    "n": "s",
+    "s": "n",
+    "e": "w",
+    "w": "e",
+}
+
+class Explorer:
+    def explore(self, num_rooms):
+        visited_directions = {} # room_id -> set(visited exits for this room_id)
+        map = {} # direction goes to
+        traversal_path = []
+        backtracking = Stack()
+        response = api.init()
+        available_exits, cooldown = response["exits"], response["cooldown"]
+
+        # We're only done after we've visited all rooms.
+        while len(visited_directions) < num_rooms:
+            print(f"Waiting {cooldown} second(s).")
+            time.sleep(cooldown)
+
+            if map.get(response["room_id"]) is None:
+                map[response["room_id"]] = {}
+
+            if visited_directions.get(response["room_id"]) is None:
+                visited_directions[response["room_id"]] = set()
+
+            unvisited_exits = [ex for ex in available_exits if ex not in visited_directions[response["room_id"]]]
+
+            if len(unvisited_exits) > 0: # we can keep exploring.
+                direction = random.choice(unvisited_exits)
+                new_response = api.move(direction)
+                map[response["room_id"]][direction] = new_response["room_id"]
+                available_exits, cooldown = new_response["exits"], new_response["cooldown"]
+                visited_directions[response["room_id"]].add(direction)
+                traversal_path.append(direction)
+
+                # Save the opposite direction of the one we just traveled.
+                # If we run into a dead end at some point, we'll need to move backwards.
+                backtracking.push((opposite_direction_mapping[direction], response["room_id"]))
+            else: # we've reached a dead end and must move backwards.
+                backtracking_direction, backtracking_room_id = backtracking.pop()
+                new_response = api.move(backtracking_direction, backtracking_room_id)
+                available_exits, cooldown = new_response["exits"], new_response["cooldown"]
+                traversal_path.append(backtracking_direction)
+
+explorer = Explorer()
+explorer.explore(500)
