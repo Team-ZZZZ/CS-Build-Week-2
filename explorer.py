25e3eca557776f4da271c1bd8e4d35337ebaf3f8
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-03-04 11:19:44 -0600

    Progress:

--- explorer.py
+++ explorer.py
@@ -13,42 +13,97 @@ opposite_direction_mapping = {
 
 class Explorer:
     def explore(self, num_rooms):
-        visited_directions = {} # room_id -> set(visited exits for this room_id)
-        map = {} # direction goes to
+        map = {}
+
+        with open("map.json", "r") as f:
+            try:
+                map = json.loads(f.read())
+            except:
+                pass
+
         traversal_path = []
         backtracking = Stack()
+
+        with open("backtracking.txt", "r") as f:
+            backtracking.stack = f.read().splitlines()
+
         response = api.init()
         available_exits, cooldown = response["exits"], response["cooldown"]
+        num_visited_rooms = 0
+
+        for room in map.values():
+            for available_exit in room["available_exits"]:
+                if available_exit in room["visited_exits"]:
+                    num_visited_rooms += 1
+                    break
 
         # We're only done after we've visited all rooms.
-        while len(visited_directions) < num_rooms:
+        while num_visited_rooms < num_rooms:
             print(f"Waiting {cooldown} second(s).")
             time.sleep(cooldown)
 
-            if map.get(response["room_id"]) is None:
-                map[response["room_id"]] = {}
+            for item_name in response["items"]:
+                if item_name == "tiny treasure" or item_name == "small treasure":
+                    continue
 
-            if visited_directions.get(response["room_id"]) is None:
-                visited_directions[response["room_id"]] = set()
+                item_response = api.take_item(item_name)
+                print(f"Waiting {item_response['cooldown']} second(s).")
+                time.sleep(item_response["cooldown"])
 
-            unvisited_exits = [ex for ex in available_exits if ex not in visited_directions[response["room_id"]]]
+            if map.get(str(response["room_id"])) is None:
+                map[str(response["room_id"])] = {
+                    "visited_exits": [],
+                    "exits": {},
+                    "available_exits": response["exits"],
+                    "title": response["title"],
+                    "description": response["description"],
+                    "coordinates": response["coordinates"],
+                }
+
+            unvisited_exits = [ex for ex in available_exits if ex not in map[str(response["room_id"])]["visited_exits"]]
 
             if len(unvisited_exits) > 0: # we can keep exploring.
                 direction = random.choice(unvisited_exits)
                 new_response = api.move(direction)
-                map[response["room_id"]][direction] = new_response["room_id"]
+                map[str(response["room_id"])]["exits"][direction] = new_response["room_id"]
                 available_exits, cooldown = new_response["exits"], new_response["cooldown"]
-                visited_directions[response["room_id"]].add(direction)
+                map[str(response["room_id"])]["visited_exits"].append(direction)
                 traversal_path.append(direction)
+
+                if map.get(str(new_response["room_id"])) is None:
+                    map[str(new_response["room_id"])] = {
+                        "visited_exits": [],
+                        "exits": {},
+                        "available_exits": new_response["exits"],
+                        "title": new_response["title"],
+                        "description": new_response["description"],
+                        "coordinates": new_response["coordinates"],
+                    }
 
                 # Save the opposite direction of the one we just traveled.
                 # If we run into a dead end at some point, we'll need to move backwards.
-                backtracking.push((opposite_direction_mapping[direction], response["room_id"]))
+                opposite_direction = opposite_direction_mapping[direction]
+                map[str(new_response["room_id"])]["exits"][opposite_direction] = response["room_id"]
+                map[str(new_response["room_id"])]["visited_exits"].append(opposite_direction)
+                backtracking.push(opposite_direction)
+
+                response = new_response
             else: # we've reached a dead end and must move backwards.
-                backtracking_direction, backtracking_room_id = backtracking.pop()
-                new_response = api.move(backtracking_direction, backtracking_room_id)
+                num_visited_rooms += 1
+                backtracking_direction = backtracking.pop()
+                new_response = api.move(backtracking_direction, map[str(response["room_id"])]["exits"][backtracking_direction])
                 available_exits, cooldown = new_response["exits"], new_response["cooldown"]
                 traversal_path.append(backtracking_direction)
+
+                response = new_response
+
+            with open("map.json", "w") as f:
+                f.write(json.dumps(map))
+            
+            with open("backtracking.txt", "w") as f:
+                f.writelines(s + "\n" for s in backtracking.stack)
+        
+        print("Success!")
 
 explorer = Explorer()
 explorer.explore(500)
